f3554d1bbc065b4dac374a11c2d3b0171f914e77
r-furutaka <rettsura@gmail.com>
2025-05-11 17:31:01 +0900

    ECRの設定まで入れる

---
+++ project/win_record_spread_sheet/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region to deploy to"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "repository_name" {
+  description = "win_record_spread_sheet"
+  type        = string
+}
