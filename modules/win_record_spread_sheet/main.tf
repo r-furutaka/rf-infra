c532f0690f87e1b8ae93ed20941ac3827be364db
r-furutaka <rettsura@gmail.com>
2025-05-12 16:11:05 +0900

    ecr.tfを分ける

--- modules/win_record_spread_sheet/main.tf
+++ modules/win_record_spread_sheet/main.tf
@@ -1,14 +1,3 @@
 provider "aws" {
   region = var.aws_region
 }
-
-resource "aws_ecr_repository" "example" {
-  name = var.repository_name
-
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
